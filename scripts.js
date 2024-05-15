228a36f9b2ca04967bbfb08db73ced19e0b86657
Tio Erico <ericorosa27@gmail.com>
2024-05-15 13:06:33 -0300

    master update 4
    adicionado a função da buy-page, falta eu concluir a organização dos materiais na página

--- scripts.js
+++ scripts.js
@@ -14,21 +14,3 @@ function searchProducts() {
         }
     }
 }
-
-/* function searchProducts() {
-    var input, filter, gallery, content, i, title;
-    input = document.querySelector('.searchbar');
-    filter = input.value.toUpperCase();
-    gallery = document.querySelector('.gallery');
-    content = gallery.getElementsByClassName('content');
-
-    for (i = 0; i < content.length; i++) {
-        title = content[i].querySelector('.product-title');
-        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
-            content[i].style.display = "";
-        } else {
-            content[i].style.display = "none";
-        }
-    }
-}
-*/
